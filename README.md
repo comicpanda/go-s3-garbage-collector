00e333109b780ade8bb9262a5536faf86e4d015a
Dev1 <isyoon@tapasmedia.co>
2015-09-24 13:40:26 -0700

    Init

---
+++ README.md
@@ -0,0 +1,1 @@
+### Clean up temporary files on S3
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"flag"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type SlackPayload struct {
+	Text      string `json:"text"`
+	Username  string `json:"username"`
+	IconEmoji string `json:"icon_emoji"`
+}
+
+const SlackAPIUrl string = "https://hooks.slack.com/services/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"
+
+var slackNotification bool
+
+func main() {
+
+	dryRun := flag.Bool("dryRun", false, "If true, data won't be deleted")
+
+	flag.Parse()
+	log.SetOutput(os.Stdout)
+
+	slackNotification = !*dryRun
+
+	if len(flag.Args()) < 1 {
+		log.Println("Usage: s3-garbage-collector filepath(e.g /home/panda/s3.log)")
+		os.Exit(1)
+	}
+
+	filepath := flag.Args()[0]
+	log.Printf("filepath : %s \n", filepath)
+
+	inFile, _ := os.Open(filepath)
+	defer inFile.Close()
+	scanner := bufio.NewScanner(inFile)
+	scanner.Split(bufio.ScanLines)
+
+	svc := s3.New(&aws.Config{Region: aws.String("us-west-2")})
+
+	for scanner.Scan() {
+		row := scanner.Text()
+		data := strings.Split(row, ",")
+		if len(data) == 2 {
+			params := &s3.DeleteObjectInput{
+				Bucket: aws.String(data[0]),
+				Key:    aws.String(data[1]),
+			}
+			svc.DeleteObject(params)
+			_, err := svc.DeleteObject(params)
+			isError(err, data[1])
+		}
+	}
+
+	defer os.Remove(filepath)
+}
+
+func isError(err error, key string) {
+	if err != nil {
+		log.Println(key)
+		if slackNotification {
+			notifyToSlack(err.Error())
+		}
+	}
+}
+
+func notifyToSlack(msg string) {
+	payload := SlackPayload{Text: msg, Username: "I'm S3 Garbage Collector", IconEmoji: ":construction_worker:"}
+	j, _ := json.Marshal(payload)
+	data := url.Values{}
+	data.Set("payload", string(j))
+	resp, _ := http.PostForm(SlackAPIUrl, data)
+	defer resp.Body.Close()
+}
